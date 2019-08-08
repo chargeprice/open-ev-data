49d1a2a926e00e98468990a79dd0c0af7c0b9896
Niklas Hoesl <nik.hoesl@hotmail.com>
2019-08-08 21:32:26 +0200

    ADD initial dataset

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Open EV Data
-Open Dataset of Electric Vehicle specs
+Open Dataset of Electric Vehicles and their specs.
+
+In contrast to ICE cars, electric vehicles have very different behavious in terms of charging and charging speed. Hence having reliable data about a car is the key for developing EV-related applications.
+
+This dataset (`data/ev-data.json`) can be freely integrated into ANY application.
+
+This is a side project of the charging price and tariff comparison platform [Chargeprice (former Plugchecker)](https://github.com/hoenic07/plugchecker).
+
+## Available Data
+
+At the moment mostly charging related data is available. Feel free to add more data if you need it!
+
+* ID: Random UUID
+* Brand
+* Model
+* Release Year: Mainly to distinquish models with the same name.
+* Variant: Bigger battery, optional faster on-board charger etc.
+* Usable Battery Size: in kWh
+* AC Charger
+  * Usable Phases: No. of usable phases for AC charging. Allowed values: 1,2,3
+  * Ports: Allowed values: `type1`, `type2`
+  * Max Power: in kW
+  * Power per Charging Point:
+* DC Charger: `null` if the car doesn't support DC charging
+  * Ports: Allowed values: `ccs`, `chademo`, `tesla_suc`, `tesla_ccs`
+  * Max Power: in kW
+  * Charging Curve: Simplified charging behaviour based on FastNed's charging curve charts. If no charging curve data is available, the default curve is assumed to be: 0%: 95% of max. DC power, 75%: max. DC power, 100%: max. AC power.
+    * percentage: Charging level of battery in percentage
+    * power: in kW
+  * Is Default Charging Curve: `true` if the charging curve is based on the default curve instead of real measured data.
+
+## Update the Data
+
+1. Request write access to: https://docs.google.com/spreadsheets/d/1jT9O6YnrRCtY5jjKDL8-3GN5DzCuB64gAi1aL3-I3Ts/edit (or make a copy)
+2. Add your changes
+3. Enable the Sheets API in the Google Cloud, [create a Google Cloud Service Account](https://support.google.com/a/answer/7378726?hl=en) and give it access to the spreadsheet
+4. Create an `.env` file, based on the `.env.sample` with the credentials of your service account
+5. Ensure that Ruby and Bundler (`gem install bundler`) is installed
+6. `cd scripts && bundle`
+7. Run `ruby update_data.rb` and get the updated `data/ev-data.json` file.
---
+++ scripts/update_data.rb
@@ -0,0 +1,127 @@
+require 'dotenv'
+require 'google/apis/sheets_v4'
+require 'multi_json'
+require 'pry'
+
+Dotenv.load('.env')
+
+FILE_NAME = '../data/ev-data.json'.freeze
+PORT_NAME = %i[type1 type2 ccs chademo tesla_suc tesla_ccs].freeze
+PORT_START_COLUMN = 9
+
+def csv_to_json_file
+  response = google_service.get_spreadsheet_values(sheet_id, 'Cars')
+  cars_hash = response.values.drop(1).map { |row| parse_car(row) }
+
+  hash = {
+    data: cars_hash,
+    meta: {
+      updated_at: Time.now.utc.iso8601,
+      overall_count: cars_hash.size
+    }
+  }
+
+  File.write(FILE_NAME, MultiJson.dump(hash, pretty: true))
+end
+
+def google_service
+  service = Google::Apis::SheetsV4::SheetsService.new
+  auth = ::Google::Auth::ServiceAccountCredentials
+         .make_creds(scope: Google::Apis::SheetsV4::AUTH_SPREADSHEETS_READONLY)
+  service.authorization = auth
+  service
+end
+
+def parse_car(row)
+  {
+    id: row[0],
+    brand: row[1],
+    model: row[2],
+    release_year: !row[3].empty? ? to_i(row[3]) : nil,
+    variant: row[4],
+    usable_battery_size: to_f(row[5]),
+    ac_charger: ac_charger(row),
+    dc_charger: dc_charger(row)
+  }
+end
+
+def ac_charger(row)
+  {
+    usable_phases: to_i(row[6]),
+    ports: [fetch_port(row, 0), fetch_port(row, 1)].compact,
+    max_power: to_f(row[7]),
+    power_per_charging_point: power_per_charging_point(row)
+  }
+end
+
+def dc_charger(row)
+  ports = [2, 3, 4].map { |idx| fetch_port(row, idx) }.compact
+  return if ports.empty?
+
+  curve = charging_curve(row)
+  max_power = to_f(row[7])
+  {
+    ports: ports,
+    max_power: curve ? curve.max_by { |v| v[:power] }[:power] : max_power,
+    charging_curve: curve || default_charging_curve(max_power, row),
+    is_default_charging_curve: !curve
+  }
+end
+
+def charging_curve(row) # rubocop:disable Metrics/MethodLength
+  data = row[15]
+  return if data.nil? || data.empty?
+
+  last_percentage = -1
+  data.split(';').map do |pair|
+    percentage, power = pair.split(',')
+    percentage = to_i(percentage)
+    power = to_i(power)
+    raise 'percentage needs to grow' if last_percentage >= percentage
+
+    last_percentage = percentage
+    { percentage: percentage, power: power }
+  end
+end
+
+def default_charging_curve(max_power, row)
+  max_ac_power = to_f(row[6])
+
+  [
+    { percentage: 0, power: (max_power * 0.95).to_i },
+    { percentage: 75, power: max_power },
+    { percentage: 100, power: max_ac_power }
+  ]
+end
+
+def fetch_port(row, column)
+  row[PORT_START_COLUMN + column] == 'x' ? PORT_NAME[column] : nil
+end
+
+def power_per_charging_point(row)
+  max_power = to_f(row[7])
+  max_phases = to_i(row[6])
+  {
+    2.3 => [max_power, 2.3].min,
+    3.7 => [max_power, 3.7].min,
+    7.4 => [max_power, 7.4].min,
+    11 => [max_power, max_phases * 3.7].min,
+    22 => [max_power, max_phases * 7.4].min,
+    43 => max_power > 22 ? max_power : [max_power, max_phases * 7.4].min
+  }
+end
+
+def to_f(value)
+  Float(value.tr(',', '.')).tap { |f| raise 'float must be >=0' unless f >= 0 }
+end
+
+def to_i(value)
+  Integer(value).tap { |i| raise 'int must be >=0' unless i >= 0 }
+end
+
+def sheet_id
+  ENV['APPLICATION_SHEET_ID']
+end
+
+csv_to_json_file
+puts "Saved to #{FILE_NAME}"
