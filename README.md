1d49d20cf098475faa20549ade46239826052b91
Niklas Hösl <niklas.hoesl@runtastic.com>
2021-02-13 20:32:31 +0100

    update data soure to contentful

--- README.md
+++ README.md
@@ -1,51 +1,53 @@
 # Open EV Data
+
 Open Dataset of Electric Vehicles and their specs.
 
-In contrast to ICE cars, electric vehicles have very different behavious in terms of charging and charging speed. Hence having reliable data about a car is the key for developing EV-related applications.
+In contrast to ICE cars, electric vehicles have very different behavious in
+terms of charging and charging speed. Hence having reliable data about a car is
+the key for developing EV-related applications.
 
-This dataset (`data/ev-data.json`) can be freely integrated into ANY application.
+This dataset (`data/ev-data.json`) can be freely integrated into ANY
+application. Mentioning Open EV Data as a source is appreciated!
 
-This is a side project of the charging price and tariff comparison platform [Chargeprice](https://www.chargeprice.app).
+This is a side project of the charging price and tariff comparison platform
+[Chargeprice](https://www.chargeprice.app).
 
 ## Available Data
 
-At the moment mostly charging related data is available. Feel free to add more data if you need it!
+At the moment mostly charging related data is available. Feel free to add more
+data if you need it!
 
 * ID: Random UUID
 * Brand
+* Vehicle Type (car, motorbike)
 * Type (BEH, PHEV)
 * Model
 * Release Year: Mainly to distinquish models with the same name.
 * Variant: Bigger battery, optional faster on-board charger etc.
 * Usable Battery Size: in kWh
-* Average Energy Consumption
+* Average Energy Consumption: in kWh/100km
 * AC Charger: Details about the on-board charger.
   * Usable Phases: No. of usable phases for AC charging. Allowed values: 1,2,3
   * Ports: Allowed values: `type1`, `type2`
   * Max Power: in kW
-  * Power per Charging Point: Charging power at common charging points. Key and Value in kW.
+  * Power per Charging Point: Charging power at common charging points. Key and
+    Value in kW.
 * DC Charger: `null` if the car doesn't support DC charging
   * Ports: Allowed values: `ccs`, `chademo`, `tesla_suc`, `tesla_ccs`
   * Max Power: in kW
-  * Charging Curve: Simplified charging behaviour based on FastNed's charging curve charts. If no charging curve data is available, the default curve is assumed to be: 0%: 95% of max. DC power, 75%: max. DC power, 100%: max. AC power.
+  * Charging Curve: Simplified charging behaviour based on various charging
+    curve charts (e.g. Fastned). If no charging curve data is available, the
+    default curve is assumed to be: 0%: 95% of max. DC power, 75%: max. DC
+    power, 100%: max. AC power.
     * percentage: Charging level of battery in percentage
     * power: in kW
-  * Is Default Charging Curve: `true` if the charging curve is based on the default curve instead of real measured data.
+  * Is Default Charging Curve: `true` if the charging curve is based on the
+    default curve instead of real measured data.
 
 ## Updating the Data
 
-### For non-developers (or lazy developers)
+The data is managed by Chargeprice via the Contentful CMS and is regularly
+updated. 
 
-Feel free to add change requests via comments to the base dataset: 
-
-https://docs.google.com/spreadsheets/d/1jT9O6YnrRCtY5jjKDL8-3GN5DzCuB64gAi1aL3-I3Ts
-
-### For developers
-
-1. Request write access to: https://docs.google.com/spreadsheets/d/1jT9O6YnrRCtY5jjKDL8-3GN5DzCuB64gAi1aL3-I3Ts/edit (or make a copy)
-2. Add your changes
-3. Enable the Sheets API in the Google Cloud, [create a Google Cloud Service Account](https://support.google.com/a/answer/7378726?hl=en) and give it access to the spreadsheet
-4. Create an `.env` file, based on the `.env.sample` with the credentials of your service account
-5. Ensure that Ruby and Bundler (`gem install bundler`) is installed
-6. `cd scripts && bundle`
-7. Run `ruby update_data.rb` and get the updated `data/ev-data.json` file.
+Please file an issue if you have a change request or reach out to
+contact@chargeprice.net
--- scripts/update_data.rb
+++ scripts/update_data.rb
@@ -1,174 +1,139 @@
-require 'dotenv'
-require 'google/apis/sheets_v4'
+# frozen_string_literal: true
+
+require 'shortuuid'
+require 'contentful'
 require 'multi_json'
 require 'pry'
+require 'dotenv'
 
 Dotenv.load('.env')
 
-FILE_NAME = '../data/ev-data.json'.freeze
-PORT_NAME = %i[type1 type2 ccs chademo tesla_suc tesla_ccs].freeze
-ALLOWED_TYPES = ["bev","phev"]
-ALLOWED_VECHICLE_TYPES=["car","motorbike","scooter"]
+FILE_NAME = '../data/ev-data.json'
+PAGE_SIZE = 100
 
-ROWS = [
-  :id,
-  :brand,
-  :vehicle_type,
-  :type,
-  :model,
-  :release_year,
-  :variant,
-  :usable_battery_size,
-  :average_consumption,
-  :ac_phases,
-  :max_ac_power,
-  :max_dc_power,
-  :plug_type1,
-  :plug_type2,
-  :plug_ccs,
-  :plug_chademo,
-  :plug_tesla_suc,
-  :plug_tesla_ccs,
-  :dc_charging_curve
-]
-
-PORT_START_COLUMN = ROWS.index(:plug_type1)
-
-def csv_to_json_file
-  response_cars = google_service.get_spreadsheet_values(sheet_id, 'Cars')
-  response_brands = google_service.get_spreadsheet_values(sheet_id, 'Brands')
-  indexed_brands = indexed_brands(response_brands)
-  cars_hash = response_cars.values.drop(1).map { |row| parse_car(row,indexed_brands) }
+def load_from_contentful
+  entries = fetch_entries
+  brands = entries.map do |v|
+    { id: ShortUUID.expand(v.model.brand.id), name: v.model.brand.name }
+  end.uniq.sort_by { |c| c[:name] }
+  vehicles = entries.map { |entry| to_model(entry) }.sort_by { |b| b[:brand] }
 
   hash = {
-    data: cars_hash,
-    brands: indexed_brands.map { |name,id| { id: id, name: name } },
+    data: vehicles,
+    brands: brands,
     meta: {
       updated_at: Time.now.utc.iso8601,
-      overall_count: cars_hash.size
+      overall_count: vehicles.size
     }
   }
 
   File.write(FILE_NAME, MultiJson.dump(hash, pretty: true))
 end
 
-def google_service
-  service = Google::Apis::SheetsV4::SheetsService.new
-  auth = ::Google::Auth::ServiceAccountCredentials
-         .make_creds(scope: Google::Apis::SheetsV4::AUTH_SPREADSHEETS_READONLY)
-  service.authorization = auth
-  service
+def fetch_entries
+  current_skip = 0
+  entries_per_page = []
+  loop do
+    current_page = client.entries(content_type: 'vehicleModelVariant', include: 2, skip: current_skip)
+    entries_per_page << current_page.to_a
+    current_page.length.zero? ? break : current_skip += PAGE_SIZE
+  end
+
+  entries_per_page.flatten
 end
 
-def parse_car(row,brands)
+def client
+  @client ||= ::Contentful::Client.new(
+    space: ENV['CONTENTFUL_SPACE_ID'],
+    access_token: ENV['CONTENTFUL_ACCESS_TOKEN']
+  )
+end
+
+def to_model(entry)
   {
-    id: field(row,:id),
-    brand: field(row,:brand),
-    vehicle_type: field(row,:vehicle_type),
-    type: field(row, :type).downcase.tap { |t| ALLOWED_TYPES.include?(t) },
-    brand_id: brands.fetch(field(row,:brand)) { raise "brand #{field(row,:brand)} not found" },
-    model: field(row,:model),
-    release_year: !field(row,:release_year).empty? ? to_i(row,:release_year) : nil,
-    variant: field(row,:variant),
-    usable_battery_size: to_f(row,:usable_battery_size),
-    ac_charger: ac_charger(row),
-    dc_charger: dc_charger(row),
+    id: ShortUUID.expand(entry.id),
+    brand: entry.model.brand.name,
+    vehicle_type: entry.model.vehicle_type,
+    type: entry.model.ev_type,
+    brand_id: ShortUUID.expand(entry.model.brand.id),
+    model: entry.model.name,
+    release_year: entry.release_year,
+    variant: entry.variant.to_s,
+    usable_battery_size: entry.battery_size.to_f,
+    ac_charger: ac_charger(entry),
+    dc_charger: dc_charger(entry),
     energy_consumption: {
-      average_consumption: to_f(row,:average_consumption)
+      average_consumption: entry.average_consumption.to_f
     }
   }
 end
 
-def indexed_brands(response_brands)
-  response_brands.values.drop(1).each_with_object({}) do |(id,name),memo|
-    memo[name]=id
-  end
-end
-
-def ac_charger(row)
+def ac_charger(entry)
   {
-    usable_phases: to_i(row,:ac_phases),
-    ports: [fetch_port(row, 0), fetch_port(row, 1)].compact,
-    max_power: to_f(row,:max_ac_power),
-    power_per_charging_point: power_per_charging_point(row)
+    usable_phases: entry.ac_phases,
+    ports: list_value(entry, :ac_ports, []),
+    max_power: entry.max_ac_power.to_f,
+    power_per_charging_point: power_per_charging_point(entry)
   }
 end
 
-def dc_charger(row)
-  ports = [2, 3, 4, 5].map { |idx| fetch_port(row, idx) }.compact
+def dc_charger(entry)
+  ports = list_value(entry, :dc_ports, [])
   return if ports.empty?
 
-  curve = charging_curve(row)
-  max_power = to_f(row,:max_dc_power)
+  curve = dc_charging_curve(entry)
+  max_power = entry.max_dc_power
   {
     ports: ports,
-    max_power: curve ? curve.max_by { |v| v[:power] }[:power] : max_power,
-    charging_curve: curve || default_charging_curve(max_power, row),
-    is_default_charging_curve: !curve
+    max_power: (curve ? curve.max_by { |v| v[:power] }[:power] : max_power).to_f,
+    charging_curve: dc_charging_curve(entry),
+    is_default_charging_curve: !entry.respond_to?(:dc_charging_curve)
   }
 end
 
-def charging_curve(row) # rubocop:disable Metrics/MethodLength
-  data = field(row,:dc_charging_curve)
-  return if data.nil? || data.empty?
+def dc_charging_curve(entry)
+  unless entry.respond_to?(:dc_charging_curve)
+    return nil unless entry.max_dc_power
 
-  last_percentage = -1
-  data.split(';').map do |pair|
-    percentage, power = pair.split(',')
-    percentage = Integer(percentage)
-    power = Integer(power)
-    raise 'percentage needs to grow' if last_percentage >= percentage
+    max_dc_power = entry.max_dc_power.to_f
+    max_ac_power = entry.max_ac_power.to_f
+    return default_charging_curve(max_dc_power, max_ac_power)
+  end
 
-    last_percentage = percentage
-    { percentage: percentage, power: power }
+  list_value(entry, :dc_charging_curve).map do |item|
+    vals = item.split(',')
+    { percentage: Integer(vals[0]), power: Float(vals[1]) }
   end
 end
 
-def default_charging_curve(max_power, row)
-  max_ac_power = to_f(row,:max_ac_power)
+def default_charging_curve(max_dc_power, max_ac_power)
   [
-    { percentage: 0, power: (max_power * 0.95).to_i },
-    { percentage: 75, power: max_power },
+    { percentage: 0, power: (max_dc_power * 0.95).round(1) },
+    { percentage: 75, power: max_dc_power },
     { percentage: 100, power: max_ac_power }
   ]
 end
 
-def fetch_port(row, column)
-  ['x','o'].include?(row[PORT_START_COLUMN + column]) ? PORT_NAME[column] : nil
-end
-
-def power_per_charging_point(row)
-  max_power = to_f(row,:max_ac_power)
-  max_phases = to_i(row,:ac_phases)
+def power_per_charging_point(entry)
+  max_power = entry.max_ac_power.to_f
+  max_phases = entry.ac_phases
   {
-    2.0 => [max_power, 2.0].min,
-    2.3 => [max_power, 2.3].min,
-    3.7 => [max_power, 3.7].min,
-    7.4 => [max_power, 7.4].min,
-    11 => [max_power, max_phases * 3.7].min,
-    16 => [max_power, max_phases * 5.4].min,
-    22 => [max_power, max_phases * 7.4].min,
-    43 => max_power > 22 ? max_power : [max_power, max_phases * 7.4].min
+    2.0 => [max_power, 2.0].min.round(1),
+    2.3 => [max_power, 2.3].min.round(1),
+    3.7 => [max_power, 3.7].min.round(1),
+    7.4 => [max_power, 7.4].min.round(1),
+    11 => [max_power, max_phases * 3.7].min.round(1),
+    16 => [max_power, max_phases * 5.4].min.round(1),
+    22 => [max_power, max_phases * 7.4].min.round(1),
+    43 => max_power > 22 ? max_power : [max_power, max_phases * 7.4].min.round(1)
   }
 end
 
-def field(row,name)
-  row[ROWS.index(name)]
+def list_value(entry, name, default = nil)
+  return default unless entry.respond_to?(name)
+
+  entry.public_send(name)
 end
 
-def to_f(row,name)
-  value = field(row,name)
-  Float(value.tr(',', '.')).tap { |f| raise 'float must be >=0' unless f >= 0 }
-end
-
-def to_i(row,name)
-  value = field(row,name)
-  Integer(value).tap { |i| raise 'int must be >=0' unless i >= 0 }
-end
-
-def sheet_id
-  ENV['APPLICATION_SHEET_ID']
-end
-
-csv_to_json_file
+load_from_contentful
 puts "Saved to #{FILE_NAME}"
